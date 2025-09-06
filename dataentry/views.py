98edafebbbbbf54adc361637b279bd8bd60276e9
EricWanjohiMuturi <emuturi124@gmail.com>
2025-09-06 09:06:19 +0300

    UI and routes definition

--- dataentry/views.py
+++ dataentry/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def import_data(request):
-    return render(request, 'views/importdata.html')
+    return render(request, 'views/dataentry/importdata.html')
